36a4f1a3d11b1219655f752988fef68355b97a7b
marco <cybermarkus1@gmail.com>
2016-10-28 17:46:14 +0200

    Update home.js

--- public/js/tpl/apps/BSFinder/templates/home.js
+++ public/js/tpl/apps/BSFinder/templates/home.js
@@ -4,7 +4,7 @@ attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow |
 var buf = [];
 with (locals || {}) {
 var interp;
-buf.push('<div id="init"><p> Welcome to LondonBusStopFinder</p><button type="button" data-action="map" class="btn btn-primary">Map</button><a href="http://twitter.com/share?url=http%3A%2F%2Fayxos.com/londonBusFinder&amp;text=Google%20Maps%20London%20BusFinder%20using%20Backbone&amp;via=ayxos"><i class="fa fa-twitter fa-lg tw"></i></a></div><div id="controls"></div><div class="mapWrapper"><div data-spin="data-spin" class="spin hide"></div><div id="map"></div></div>');
+buf.push('<div id="init"><p> Welcome to LondonBusStopFinder</p><button type="button" data-action="map" class="btn btn-primary">Map</button><a href="http://twitter.com/share?url=http%3A%2F%2Fayxos.github.io/LondonBusStopFinder&amp;text=Google%20Maps%20London%20BusFinder%20using%20Backbone&amp;via=ayxos"><i class="fa fa-twitter fa-lg tw"></i></a></div><div id="controls"></div><div class="mapWrapper"><div data-spin="data-spin" class="spin hide"></div><div id="map"></div></div>');
 }
 return buf.join("");
 };
